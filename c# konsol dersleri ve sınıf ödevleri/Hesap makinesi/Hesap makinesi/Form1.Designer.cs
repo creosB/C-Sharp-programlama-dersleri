7e1cb3ca81aec453d88f1dc7e3bd3a790b66875a
creosB <baris0631@hotmail.com>
2020-12-24 01:44:22 +0300

    Add files via upload

---
+++ c# konsol dersleri ve sınıf ödevleri/Hesap makinesi/Hesap makinesi/Form1.Designer.cs
@@ -0,0 +1,158 @@
+﻿namespace Hesap_makinesi
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.sonuc = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.sayi1 = new System.Windows.Forms.TextBox();
+            this.sayi2 = new System.Windows.Forms.TextBox();
+            this.cikarma = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F);
+            this.label1.Location = new System.Drawing.Point(27, 75);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(73, 25);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Sayı 1:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F);
+            this.label2.Location = new System.Drawing.Point(27, 123);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(73, 25);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Sayı 2:";
+            // 
+            // sonuc
+            // 
+            this.sonuc.AutoSize = true;
+            this.sonuc.Location = new System.Drawing.Point(406, 84);
+            this.sonuc.Name = "sonuc";
+            this.sonuc.Size = new System.Drawing.Size(35, 13);
+            this.sonuc.TabIndex = 2;
+            this.sonuc.Text = "label3";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F);
+            this.label4.Location = new System.Drawing.Point(303, 75);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(95, 25);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Toplama:";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(100, 178);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(139, 42);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "İşlemi yap";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // sayi1
+            // 
+            this.sayi1.Location = new System.Drawing.Point(112, 81);
+            this.sayi1.Name = "sayi1";
+            this.sayi1.Size = new System.Drawing.Size(113, 20);
+            this.sayi1.TabIndex = 5;
+            // 
+            // sayi2
+            // 
+            this.sayi2.Location = new System.Drawing.Point(112, 129);
+            this.sayi2.Name = "sayi2";
+            this.sayi2.Size = new System.Drawing.Size(113, 20);
+            this.sayi2.TabIndex = 6;
+            // 
+            // cikarma
+            // 
+            this.cikarma.AutoSize = true;
+            this.cikarma.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F);
+            this.cikarma.Location = new System.Drawing.Point(303, 129);
+            this.cikarma.Name = "cikarma";
+            this.cikarma.Size = new System.Drawing.Size(91, 25);
+            this.cikarma.TabIndex = 8;
+            this.cikarma.Text = "Çıkarma:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(406, 138);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(35, 13);
+            this.label5.TabIndex = 7;
+            this.label5.Text = "label3";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(676, 445);
+            this.Controls.Add(this.cikarma);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.sayi2);
+            this.Controls.Add(this.sayi1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.sonuc);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label sonuc;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox sayi1;
+        private System.Windows.Forms.TextBox sayi2;
+        private System.Windows.Forms.Label cikarma;
+        private System.Windows.Forms.Label label5;
+    }
+}
+
---
+++ c# konsol dersleri ve sınıf ödevleri/Hesap makinesi/Hesap makinesi/Form1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Hesap_makinesi
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int a,b,toplam,cik;
+
+            a = Convert.ToInt16(sayi1.Text);
+            b = Convert.ToInt16(sayi2.Text);
+            toplam = (a + b);
+            cik = (a - b);
+
+            sonuc.Text = toplam.ToString();
+            label5.Text = cik.ToString();
+
+        }
+    }
+}
